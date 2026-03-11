16d9f3eb674336363a79251eb4552036493289e7
Danilo Reilly <daniloreilly3@gmail.com>
2026-03-10 21:52:46 -0300

    feat(backend): implement database connection and auto-migration

---
+++ backend/database.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/daniloreilly/encurtador-links/backend/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = database.AutoMigrate(&models.Link{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	DB = database
+}
